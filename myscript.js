908ec60a48bae890da169caba90f565429bcc57b
mattesalva93 <salvalaggiomatteo93@gmail.com>
2022-01-13 14:13:54 +0100

    all done

---
+++ myscript.js
@@ -0,0 +1,8 @@
+let app = new Vue({
+    el: "#container",
+    data: {
+        messaggio: "VueJS mi fa più paura del covid",
+        classi: "text-center mt-5",
+        link_immagine: "https://pbs.twimg.com/profile_images/1204769399980396550/z5OuamU8_400x400.jpg"
+    }
+});
